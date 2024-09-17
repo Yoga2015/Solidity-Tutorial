879e7e981c5c79cc1c708aa7fba31e0431963f33
Yoga2015 <yoga3333@126.com>
2024-09-17 23:17:29 +0800

    分析 Solidity 中 的 函数

---
+++ contracts/solidity_by_example/004Function.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Function{
+
+    // 定义 一个 状态变量 number，初始化 为 6。
+    uint256 public num = 6;
+
+    // 定义一个 add() 函数，每次调用会让 number 增加 1。 只能由 合约外部 调用（访问）
+    function add() external{
+        num = num + 1;
+    }
+
+    // pure 
+    // 如果 addPure() 函数 被标记 为 pure，就会报错。因为 pure 是不配 读取合约里的状态变量的，更不配 改写。
+    function addPure(uint256 pureNum) external pure returns (uint256 newNum) {
+        newNum = pureNum + 1;
+    }
+
+    // view
+    // 如果 addView() 函数被标记为 view，比如 function add() external view，也会报错。因为 view 能读取，但不能够改写状态变量。
+    function addView() external view returns (uint256 viewNum){
+        viewNum = num + 1;
+    }
+
+    // internal 
+    // 定义一个 internal 的 subtract() 函数，每次 调用 使得 num 变量减少 1，更改了 状态变量num.
+    // 由于 internal 函数 只能由 合约内部 调用，因此我们必须 再定义一个 external 的 subtractCall() 函数，
+    // 通过 它 间接调用 内部 的 subtract() 函数
+    function subtract() internal {
+        num  =  num - 1;
+    }
+
+    // external
+    // 合约内的函数 可以 调用 内部函数
+    function subtractCall() external {
+        subtract();
+    }
+
+    // 定义 一个 external payable 的 substractPayable() 函数，间接 的 调用 subtract() ,
+    // 并且 返回 合约里 的 ETH余额（this 关键字可以 让 我们 引用 合约地址）。 可以尝试在调用 minusPayable() 时往合约里转入1个 ETH。
+    function substractPayable() external payable  returns (uint256 balance){
+
+        subtract();
+
+        balance = address(this).balance;
+
+    }
+
+    // 返回值： return 和  returns
+    // - returns：跟在 函数名 后面，用于 声明 返回的变量类型 及 变量名。
+    // - return： 用于 函数主体 中，返回 指定 的 变量。
+    function returnMultiple() public pure returns (uint256,bool,uint256[3] memory){
+
+        return (1,true,[uint256(1),6,8]);
+
+    }
+
+    // 命名式返回
+    // 可以在 returns 中标明返回变量的名称。Solidity 会初始化这些变量，并且自动返回这些函数的值，无需使用 return。
+    function returnNamed() public pure returns (uint256 _number,bool _bool,uint256[3] memory _array){          
+
+        // 命名式返回
+        // _number = 2;
+        // _bool = false;
+        // _array = [uint256(3),7,4];
+
+        // 命名式返回，依然支持return
+        return (2,false,[uint256(3),7,4]);
+
+    }
+
+    // 读取返回值，解构式赋值
+    function readReturn() public pure {
+
+        // 读取全部返回值
+        uint256 _number;
+        bool _bool;
+        uint256[3] memory _array;
+        (_number,_bool,_array) = returnNamed();
+
+        // 读取部分返回值，解构式赋值
+        (,_bool,) = returnNamed();
+
+    }
+
+}
+
+
+
+// function <function name> (<parameter types>) {internal|external|public|private} [pure|view|payable] [returns (<return types>)]
