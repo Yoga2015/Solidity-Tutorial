7125870683d2b7c0f0bce21c57e31b9ef9c23cfb
Yoga2015 <yoga3333@126.com>
2024-10-01 23:51:47 +0800

    增加注释

--- contracts/solidity_by_example/008Mapping.sol
+++ contracts/solidity_by_example/008Mapping.sol
@@ -2,18 +2,19 @@
 
 pragma solidity ^0.8.13;
 
-contract Mapping{
+contract MappingDemo{
 
-    // 声明 映射 的 格式为 mapping(_KeyType => _ValueType)，其中_KeyType 和 _ValueType 分别是 Key 和 Value 的 变量类型。
+    // 创建（声明） 映射 的 格式为 mapping(KeyType => ValueType)，其中 KeyType 和 ValueType 分别是 Key 和 Value 的 变量类型。
 
     mapping(uint => address) public idToAddress;  // id 映射 到 地址
 
     mapping(address => address) public swapPair;  // 币对的映射，地址 到 地址
 
 
-    // 规则1：映射 的 映射键_KeyType 只能选择 Solidity 内置的 值类型，比如uint，address等，不能用自定义的结构体。
-    // 下面这个例子会报错，因为 映射键_KeyType 使用了 我们 自定义的结构体：
-    // mapping 中 只允许基本类型、用户定义的值类型、契约类型 或 枚举 作为 映射键。
+    // 规则1：映射 的 映射键_KeyType 只能选择 Solidity 内置的 值类型，比如 uint，address 等，不能用自定义的结构体。
+    // mapping 中 只允许基本类型、用户定义 的 值类型、契约类型 或 枚举 作为 映射键。
+
+    // 下面这个例子会报错，因为 KeyType 使用了 我们 自定义 的 结构体：
     struct Student{
         uint256 id;
         uint256 score;
@@ -21,6 +22,8 @@ contract Mapping{
 
     // TypeError: Only elementary types, user defined value types, contract types or enums are allowed as mapping keys.
     // mapping(Student => uint) public myMap;
+
+
 
     // 规则4：给 映射 新增 键值对 的 语法为 _Var[_Key] = _Value，其中 _Var 是 映射变量名，_Key 和_Value 对应新增的键值对。
 
@@ -33,14 +36,15 @@ contract Mapping{
 }
 
 
-//  Mapping 映射类型 是 一种 高效且灵活、 类似于 哈希表 的 数据结构，用于 存储 和 检索 键值对（Key-Value pairs）。
+// Mapping 映射类型 是 一种 高效且灵活、 类似于 哈希表 的 数据结构，用于 存储 和 检索 键值对（Key-Value pairs）。
 
-//  使用 mapping 来 管理 和 操作 数据集合，但 mapping 不可遍历、不可比较。
+// 使用 mapping 来 管理 和 操作 数据集合，但 mapping 不可遍历、不可比较。
 
+// Solidity 中， 存储 键值对 的 数据结构，可以理解为 哈希表。
 
 
 // 在 智能合约 中 如何 高效地 存储 和 检索 数据？
 
 // 通过 键值对 的方式，mapping 允许 开发者 通过 键（Key）来查询 对应的值（Value），比如：
 
-// 通过 一个人 的 id 来查询 他的钱包地址，这在处理用户信息、权限管理、资源分配 等场景中尤为重要。
+// 通过 一个人 的 id 来查询 他的钱包地址，这在处理 用户信息、权限管理、资源分配 等场景中 尤为重要。
--- contracts/solidity_by_example/009MyMapping.sol
+++ contracts/solidity_by_example/009MyMapping.sol
@@ -1,41 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+// NestedMapping 合约中 定义了 一个 嵌套的 映射 数据结构，用于存储 地址 与 代币余额 之间 的 关系。
 contract NestedMapping {
 
-    // 声明一个嵌套映射，其中 外层映射 的 键 是 地址类型，值 是 另一个映射  
-    // 内部映射 的 键 是 字符串类型，值 是 整型  
-    mapping(address => mapping(string => uint256)) public balances;  
-  
-    //  setBalance 函数 来设置 内部映射中 的 值  
-    function setBalance(address ownerValue, string memory tokenValue, uint256 _amount) public {  
+  // 声明一个 公开 的 嵌套映射，允许 外部访问 和 查询余额信息 。
+  // 其中 外层映射 的 键 是 地址类型（address）， 代表 代币 的 所有者 ； 值 是 一个映射 ，是 一个内部映射，
+  // 该 内部映射 的 键 是 字符串类型，代表 代币的名称 或 标识符，而 值 是 无符号的256位整数类型，代表 代币的余额 。 
+  mapping(address => mapping(string => uint256)) public balances; 
 
-        balances[ownerValue][tokenValue] = _amount;  
+  // 定义 setBalance函数, 允许 外部调用者 设置 或 更新 特定地址 和 代币名称 的 余额。 (通过 嵌套映射的键 直接设置 余额值)
+  // setBalance函数 接收 的 参数 一一对应 上面的 嵌套映射 的 结构 
+  //      ownerValue（address）：代币的所有者地址。
+  //      tokenValue（string memory）：代币的名称或标识符。
+  //      amountValue（uint256）：要设置的余额数量。
+  function setBalance(address ownerValue, string memory tokenValue, uint256 amountValue) public { 
 
-    }  
-  
-    // getBalance 函数 来获取 内部映射中 的 值  
-    function getBalance(address ownerValue, string memory tokenValue) public view returns (uint256) { 
+    balances[ownerValue][tokenValue] = amountValue; 
+  } 
 
-        return balances[ownerValue][tokenValue];  
+  // 定义 getBalance函数  允许 外部调用者 查询（获取） 特定地址 和 代币名称 的 余额
+  // getBalance函数 接收 的 参数 ：
+  //      ownerValue（address）：代币的所有者地址。
+  //      tokenValue（string memory）：代币的名称 或 标识符。
+  function getBalance(address ownerValue, string memory tokenValue) public view returns (uint256) { 
 
-    }  
-  
-    // 示例：在构造函数中初始化一些数据  
-    constructor() {  
+    return balances[ownerValue][tokenValue]; 
+  } 
 
-        // 假设有两个地址和两种代币  
-        address owner1 = 0x32E649d3385182b41aebbDD58032B3939BCbA53B;  // 示例地址1  
-        address owner2 = 0xA5d68CE183B2bdCC48Fa283D55Bb4E1EdbA67F43;  // 示例地址2  
-        string memory tokenA = "TokenA";  
-        string memory tokenB = "TokenB";  
-  
-        // 为两个地址分别设置代币A和代币B的余额  
-        setBalance(owner1, tokenA, 100);  
-        setBalance(owner1, tokenB, 200);  
-        setBalance(owner2, tokenA, 50);  
-        setBalance(owner2, tokenB, 150);  
+  // 定义 构造函数 用于 在合约部署时 初始化 一些示例数据， 实现 通过 setBalance函数 为 两个示例地址 分别设置了 两种代币的余额
+  constructor() { 
 
-    }  
+    // 假设有 两个地址 和 两种代币  
+    address owner1 = 0x32E649d3385182b41aebbDD58032B3939BCbA53B; // 示例地址1  
+    address owner2 = 0xA5d68CE183B2bdCC48Fa283D55Bb4E1EdbA67F43; // 示例地址2  
+    string memory tokenA = "TokenA"; 
+    string memory tokenB = "TokenB"; 
+    
+    // 为两个地址分别设置代币A和代币B的余额  
+    setBalance(owner1, tokenA, 100); 
+    setBalance(owner1, tokenB, 200);
+    setBalance(owner2, tokenA, 50); 
+    setBalance(owner2, tokenB, 150); 
+
+  } 
 
 }
