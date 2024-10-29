36cea52b880a20ca2058ce0292bd2ab49878280f
Yoga2015 <yoga3333@126.com>
2024-10-29 12:26:48 +0800

    利用 using for 指令 或  通过 库合约名称 调用函数  来使用 库合约Strings

---
+++ contracts/solidity_by_example/034Library.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+library Strings {
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     */
+    function toString(uint256 value) public pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+     */
+    function toHexString(uint256 value) public pure returns (string memory) {
+        if (value == 0) {
+            return "0x00";
+        }
+        uint256 temp = value;
+        uint256 length = 0;
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+        return toHexString(value, length);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+     */
+    function toHexString(uint256 value, uint256 length) public pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
+    }
+}
+
+// 如何使用 库合约
+
+contract UseLibrary{
+
+    // 1、利用 using for 指令  来使用 库合约
+    using Strings for uint256;
+
+    function getString1(uint256 _number) public pure returns (string memory){
+
+        // 库合约中的函数 会自动添加为 uint256类型变量 的 成员
+        return _number.toHexString();
+    }
+
+    // 2、通过 库合约名称 调用函数  来使用 库合约
+    function getString2(uint256 _number) public pure returns (string memory){
+
+        return Strings.toHexString(_number);
+    }
+
+}
+
+// 部署 UseLibrary合约 并输入 170 测试一下，两种方法 均能返回 正确的 16进制string “0xaa”。证明我们调用库合约成功！
