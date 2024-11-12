54a2a44e803a8f404c9536de55df65157bbe9ec8
Yoga2015 <yoga3333@126.com>
2024-11-12 23:58:53 +0800

    调用已部署的合约​

---
+++ contracts/solidity_by_example/040OtherContract.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract OtherContract {
+    // 定义一个名为 _x 的 私有变量，类型为uint256，初始化为 0
+    uint256 private _x = 0;
+
+    // Log事件 ，记录日志，收到 ETH 后 ，记录 amount 和 gas
+    event Log(uint amount, uint gas);
+
+    // 返回 当前合约 的地址的余额
+    function getBalance() public view returns (uint) {
+        // address(this) 获取 当前合约的地址，.balance 是 该地址的 余额属性。
+        return address(this).balance;
+    }
+
+    // 可以 调整 状态变量 _x 的 函数，并且 可以 往合约 转 ETH (payable)
+    function setX(uint256 x) external payable {
+        _x = x;
+
+        // 如果 转入 ETH , 则释放Log 事件
+        if (msg.value > 0) {
+            emit Log(msg.value, gasleft());
+        }
+    }
+
+    // 读取 _x
+    function getX() external view returns (uint x) {
+        x = _x;
+    }
+}
+
+// OtherContract 合约 包含一个状态变量_x，一个事件Log 在 收到ETH时 触发，三个函数：
+
+// getBalance(): 返回合约ETH余额。
+// setX(): external payable函数，可以设置_x的值，并向合约发送ETH。
+// getX(): 读取_x的值。
+
+contract CallContract {
+    function callSetX(address _Address, uint256 x) external {
+        OtherContract(_Address).setX(x);
+    }
+
+    function callGetX(OtherContract _Address) external view returns (uint x) {
+        x = _Address.getX();
+    }
+
+    function callGet2(address _Address) external view returns (uint x) {}
+}
