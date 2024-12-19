b889eb4042378eadcefb21db546be7ef2f0bca8d
Yoga2015 <yoga3333@126.com>
2024-12-19 23:57:26 +0800

    在 创建新合约中 使用 try-catch (合约 创建被视为 external call)

--- contracts/solidity_by_example/050TryCatch.sol
+++ contracts/solidity_by_example/050TryCatch.sol
@@ -52,6 +52,8 @@ contract TryCatch {
     }
 }
 
-// 在Solidity 中 使用 try-catch 来处理 智能合约运行中 的 异常：
-// - try-catch 只能用于 外部合约调用 和 合约创建。
+// 在 Solidity 中 使用 try-catch 来处理 智能合约运行中 的 异常：
+
+// - try-catch 只能 用于 外部合约函数调用 和 合约创建 。
+
 // - 如果 try 执行成功，返回 变量 必须 声明，并且 与 返回的变量类型 相同。
