c2767fa21d34d455a1c44f907969a35d6b4ec59d
Yoga2015 <yoga3333@126.com>
2024-12-27 01:17:12 +0800

    修改示例

--- contracts/solidity_by_example/BytesExample.sol
+++ contracts/solidity_by_example/BytesExample.sol
@@ -3,24 +3,31 @@
 pragma solidity ^0.8.13;
 
 contract BytesExample {
-    bytes public data;
+    bytes public data; // 使用 bytes 关键字 来创建 一个 不定长字节数组 data，其 长度 在声明之后 可以改变。
 
-    function setData(bytes newData) public {
+    // 设置data的值
+    function setData(bytes calldata newData) public {
         data = newData;
     }
 
-    function appendData(byte newDataByte) public {
-        data.push(newDataByte);
+    // 向data数组的末尾添加一个字节
+    function appendData(bytes calldata newDataByte) public {
+        // 在Solidity 0.8.0之后，bytes和string类型的.push()方法已被移除.故使用 abi.encodePacked() 来模拟 添加 字节的效果
+        data = abi.encodePacked(data, newDataByte);
     }
 
-    function getDataLength() public view returns (uint) {
+    // 获取data数组的长度
+    function getDataLength() public view returns (uint256) {
         return data.length;
     }
 }
 
-// 不定长字节数组 bytes
-// 不定长字节数组 是 Solidity中的一种 特殊的 "数组" 数据类型，其元素类型是字节，且数组的长度可以在运行时动态变化。
-// 在Solidity中，为了处理不同长度的数据，尤其是二进制数据，引入了不定长字节数组。
-// "不定长字节数组 " 这种数据类型允许开发者在不知道数据确切长度的情况下，灵活地存储和处理数据。
-// 不定长字节数组能够存储任意长度的二进制数据，如哈希值、加密数据、图像数据等。
-// 定长数组 无法适应 数据长度 的 不确定性，而不定长字节数组则解决了这一问题，提供了更高的灵活性和适应性。
+// 什么 是 不定长字节数组 bytes
+
+// 在Solidity中，为了处理 不同长度 的 数据，尤其是 二进制数据，引入了 不定长字节数组。
+
+// 不定长字节数组 是Solidity中的一种 引用类型 数据类型 ，其 元素类型 是 字节，且 数组的长度 可以在运行时动态变化
+
+// 不定长字节数组  允许 开发者 在 不知道 数据 确切长度 的 情况下，灵活地 存储 和 处理 数据。
+
+// 不定长字节数组  主要用来  存储  和  处理  任意长度 的 二进制数据 。
