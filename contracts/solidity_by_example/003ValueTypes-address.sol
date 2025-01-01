ea4b43a050857d5d77451e96f3389e4e9f3f340b
Yoga2015 <yoga3333@126.com>
2025-01-01 23:06:32 +0800

    修改并增加  payable类型地址 案例

--- contracts/solidity_by_example/003ValueTypes-address.sol
+++ contracts/solidity_by_example/003ValueTypes-address.sol
@@ -24,3 +24,27 @@ contract Payment {
         return address(this).balance; // 返回合约当前的以太币余额
     }
 }
+
+// 在这个例子中，我们创建了一个名为Payment的合约，
+// 它 接受一个 payable address地址 作为 构造函数参数，并将 其 设置为 收款人。
+// 然后，我们提供了一个withdraw函数，只有收款人 才能 调用它 来提取 资金。
+
+contract Payment2 {
+    address payable public payee; // 收款人地址
+
+    // 构造函数，接受一个payable地址作为收款人
+    constructor(address payable _payee) {
+        payee = _payee;
+    }
+
+    // withdraw函数，只能由收款人调用
+    function withdraw(uint256 amount) public {
+        require(msg.sender == payee, "Only the payee can withdraw funds."); // 检查调用者是否为收款人
+        payee.transfer(amount); // 将资金转移到收款人地址
+    }
+
+    // 可选：查看合约余额的函数
+    function getBalance() public view returns (uint256) {
+        return address(this).balance; // 返回合约当前的以太币余额
+    }
+}
