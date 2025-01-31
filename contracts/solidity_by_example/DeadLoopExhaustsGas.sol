1c74fb4c2efb484db304b09223fe1213091f0be9
Yoga2015 <yoga3333@126.com>
2025-02-01 00:04:11 +0800

    修改注释

--- contracts/solidity_by_example/DeadLoopExhaustsGas.sol
+++ contracts/solidity_by_example/DeadLoopExhaustsGas.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.25;
 contract ForeverTest {
     uint256 public i = 0;
 
-    // 当发送的所有 gas 被用尽时，会导致你的交易失败。
-    // 状态更改将被撤销。
-    // 已消耗的 gas 不会退还。
+    // 当发送的所有 gas 被用尽时，会导致你的交易失败。状态更改将被撤销。已消耗的 gas 不会退还。
     // Remix测试会报错：
     // transact to ForeverTest.forever errored: Error occurred: out of gas.
 
     // out of gas
-    //  The transaction ran out of gas. Please increase the Gas Limit.
+    // The transaction ran out of gas. Please increase the Gas Limit.
     function forever() public {
         while (true) {
             i += 1;
