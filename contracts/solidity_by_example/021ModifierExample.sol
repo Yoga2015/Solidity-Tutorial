7e43e15a02be000b75a0af51e76ed7176f6d425f
Yoga2015 <yoga3333@126.com>
2025-02-14 21:57:27 +0800

    在 changeOwner 函数中添加事件，以便跟踪所有者变更

--- contracts/solidity_by_example/021ModifierExample.sol
+++ contracts/solidity_by_example/021ModifierExample.sol
@@ -2,38 +2,43 @@
 pragma solidity ^0.8.13;
 
 contract FunctionModifier {
+    // 声明了一个公共状态变量 owner，将用于 存储 合约所有者 的 地址。
     address public owner;
 
+    event OwenrChanged(address indexed _oldOwner, address indexed _newOwner);
+
+    // 在合约部署时 初始化 owner 变量。 [msg.sender 是 部署合约的地址（即合约的创建者）]
     constructor() {
-        owner = msg.sender; // 初始化时， 将 交易发送方 设置为 合约 的 所有者
+        owner = msg.sender; // 将 msg.sender 赋值给 owner，表示 合约的初始所有者 是 部署者
     }
 
-    // 通过 关键字 modifier  来定义 一个修饰器 onlyOwner
+    // 1、使用 关键字 modifier  定义 一个修饰器 onlyOwner，它用于 权限控制，确保 只有合约所有者 可以执行 某些敏感操作。
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner"); // 检查 调用者 是否为 owner地址
+        require(msg.sender == owner, "Not owner"); // 检查 调用者 权限，防止未授权的操作，如果权限检查失败，交易会被回滚
 
-        _; // 如果是的话，继续执行 回changeOwner函数 主体；否则报错 并 revert交易
+        _; // 如果是，继续执行被修饰函数的主体（ _ 表示 被修饰函数 的 执行位置）。
+        // 如果不是，抛出错误信息 "Not owner" 并 回滚交易（revert）。
     }
 
-    // 带有 onlyOwner修饰符 的 函数 只能被 owner地址调用
+    // 2、使用 onlyOwner 修饰器，确保只有 当前owner 才能调用 该函数，如果调用者是 owner,，则将 owner 更新为 _newOwner
     function changeOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "Invalid address"); // 检查 _newOwner 是否为 有效地址（非零地址）
+
+        emit OwenrChanged(owner, _newOwner); //  记录 所有者 变更
+
         owner = _newOwner;
     }
 }
 
-// 上面例子中，定义一个叫做 onlyOwner 的 modifier，然后 定义了 一个 changeOwner 函数 并添加 onlyOwner 修饰器。
+// 上面例子中，定义一个叫做 onlyOwner 的 modifier，然后 定义了 一个 changeOwner 函数 并添加 onlyOwner 修饰器, 通过 onlyOwner 修饰器 实现了 权限限制
 
-// 调用 changeOwner 函数 可以改变 合约 的 owner，但是由于 onlyOwner 修饰器 的存在，只有 原先的 owner 可以调用，
+// 通过 修饰器 onlyOwner 确保 只有当前所有者 可以调用 changeOwner 函数,调用 changeOwner 函数 可以改变 合约 的 owner
 
-// 别人调用 就会报错，这也是 最常用的 控制 智能合约 权限 的 方法。
+// Solidity  中 的 modifier 主要用来 修饰 函数 的。     （有点像 vue.js 的 路由守卫 ）
 
-// msg.sender 是一个 全局变量，它代表了  当前正在执行函数的 发送者的地址  或  当前正在执行交易的 发送者的地址。
+// 使用 modifier 可以 在函数 调用前  或  调用后  执行特定的代码，如：扩展功能、权限控制、条件检查、代码重用 等 场景
 
-// modifier 主要用来 修饰 函数 的。
-
-// modifier 就像 钢铁侠的智能盔甲，穿上它 的 函数 会带有 某些特定的行为。
-
-// modifier 的 主要使用场景 是 运行函数前 的 检查，例如地址，变量，余额等。
+// 注意：Solidity 不允 许修饰器（modifier）重载。
 
 // 在Solidity修饰符中，_（下划线）代表 被修饰函数 的 原始（主体）代码块。
 
