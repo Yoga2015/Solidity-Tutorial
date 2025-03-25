e4ff59a2a4cf68d60d003c0e0b565f4e2aa1076a
Yoga2015 <yoga3333@126.com>
2025-03-25 23:56:02 +0800

    使用结构体（struct）定义用户信息模板，实现一个基础的用户信息管理合约

--- contracts/solidity_by_example/007UserInfoStruct.sol
+++ contracts/solidity_by_example/007UserInfoStruct.sol
@@ -1,37 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// 定义一个 UserInfo结构体 来存储 用户信息
-struct UserInfo{
-    uint age;
-    string name;
-    string email;
-}
+// 定义一个用户信息管理合约
+contract UserContract {
 
-// 创建 UserContract 合约
-contract UserContract{
-
-    // 创建 一个 UserInfo类型 的 数组users 来存储 多个用户信息
-    UserInfo[] public users;   // 用户数组
-
-    // addUser函数 可以 添加新用户
-    function addUser(uint ageVuale, string memory nameValue,string memory emailValue) public {
-
-        // 在函数内部 实例化 结构体 时，需要明确 指定 存储位置
-        UserInfo memory newUser = UserInfo(ageVuale,nameValue,emailValue);
-
-        users.push(newUser);     // 将 新用户 添加到 数组中
-
+    // 定义 用户信息结构体，包含 三个字段
+    struct UserInfo {
+        uint age;        // 用户年龄，使用 无符号整数类型 uint
+        string name;     // 用户姓名，使用 字符串类型 string
+        string email;    // 用户邮箱，使用 字符串类型 string
     }
 
+    // 声明 一个动态数组 来存储 所有用户信息 （可以被外部访问）
+    UserInfo[] public users;   
 
-    // getUser函数 可以 根据索引 获取 用户信息
-    function getUser(uint index) public view  returns (uint,string memory,string memory){
+    // 添加 新用户的函数 （可以被外部调用）
+    function addUser(uint ageVuale, string memory nameValue, string memory emailValue) public {
 
-        UserInfo memory user = users[index];
-
-        return (user.age,user.name,user.email);
-
+        // 创建新的用户信息实例  （memory 关键字表示这个实例临时存储在内存中）
+        UserInfo memory newUser = UserInfo(ageVuale, nameValue, emailValue);
+   
+        users.push(newUser);  // 将 新用户信息 添加到 用户数组 中
     }
 
+    // 查询 用户信息的函数 （只读函数，不修改状态）     returns: 指定返回值类型，这里返回年龄、姓名和邮箱
+    function getUser(uint index) public view returns (uint, string memory, string memory) {
+         
+        UserInfo memory user = users[index];    // 从 数组 中 获取 指定索引的用户信息
+   
+        return (user.age, user.name, user.email);  // 返回 用户的所有信息
+    }
 }
