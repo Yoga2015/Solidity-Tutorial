9ce9c4e61132b4139c2bbfe059acca36d38bb029
Yoga2015 <yoga3333@126.com>
2025-05-07 23:23:38 +0800

    修改注释

--- contracts/solidity_by_example/001HelloWorld.sol
+++ contracts/solidity_by_example/001HelloWorld.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT    // 这行 是 注释，说明代码所使用的软件许可（license），这里使用的是 MIT 许可。
+// SPDX-License-Identifier: MIT    
+// 上面这行 “注释”，说明 代码 所使用的软件许可（license），这里使用的是 MIT 许可。
 
 pragma solidity ^0.8.13;    // 这行代码 表示 源文件将不允许小于 0.8.13 版本 或 大于等于 0.9.0 的编译器编译。  Solidity 语句以分号（;）结尾。
 
