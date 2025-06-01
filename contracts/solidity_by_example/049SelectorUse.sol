ce2b23f50545735e2755e4c6664ca16950440ef0
Yoga2015 <yoga3333@126.com>
2025-06-01 22:52:01 +0800

    staticcall的使用场景和限制

--- contracts/solidity_by_example/049SelectorUse.sol
+++ contracts/solidity_by_example/049SelectorUse.sol
@@ -1,32 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// 使用 selector 来调用 目标函数
+// 下面例子展示了如何使用 selector 来调用目标合约中的函数。
+// 回调合约示例
 contract CallbackContract {
+    /**
+     * @dev 回调函数示例，将输入值乘以2返回
+     * @param _data 输入的无符号整数
+     * @return uint 返回_data乘以2的结果
+     */
     function callbackFunction(uint _data) external pure returns (uint) {
         return _data * 2;
     }
 }
 
+// 调用者合约示例
 contract CallerContract {
-    function callCallback(
-        address _callbackContract,
-        uint _data
-    ) external view returns (uint) {
-        bytes4 selector = bytes4(keccak256("callbackFunction(uint)")); // 计算函数选择器
 
-        // 调用回调函数
+    /**
+     * @dev 使用selector调用目标合约的函数
+     * @param _callbackContract 目标合约地址、 _data 要传递给回调函数的数据
+     * @return uint 返回回调函数的执行结果
+     */
+    function callCallback(address _callbackContract, uint _data) external view returns (uint) {
+       
+        bytes4 selector = bytes4(keccak256("callbackFunction(uint)"));     // 计算目标函数的selector
+        
+        // 使用staticcall进行调用（只读操作）
         (bool success, bytes memory result) = _callbackContract.staticcall(
-            abi.encodePacked(selector, _data)
+            abi.encodePacked(selector, _data)  // 拼接selector和参数数据
         );
 
-        require(success, "Callback failed");
-
-        return abi.decode(result, (uint)); // 解析回调结果
+        require(success, "Callback failed");     // 检查调用是否成功
+  
+        return abi.decode(result, (uint));   // 解码并返回调用结果
     }
 }
 
-// 在例子中，CallerContract 通过 函数选择器 找到 CallbackContract合约中 的 callbackFunction函数，并调 用它，这展示了 selector 在智能合约交互中 的 应用。
+// 在上面例子中，CallerContract 通过 函数选择器 找到 CallbackContract合约中 的 callbackFunction函数，并调 用它，这展示了 selector 在智能合约交互中 的 应用。
+
 
 // 什么是 selector
 
@@ -38,16 +50,17 @@ contract CallerContract {
 
 // 函数签名 则是 “函数名（逗号分隔的参数类型）”的组合。
 
+
 // selector的由来
 
 // selector的由来 可以追溯到 智能合约 的 函数调用机制。
 
-// 在 以太坊 等 区块链平台上，智能合约之间的交互 是通过 发送 包含 函数签名 和 参数 的 交易 来实现的。
+// 在 以太坊 等 区块链平台上，智能合约之间的交互 是通过发送 包含 函数签名 和 参数 的 交易 来实现的。
 
 // 为了确定要调用的函数，区块链节点 会根据 交易数据中的前4个字节（即selector）与 合约中 定义的函数签名 进行 匹配。
 
 // selector的主要作用
 
-// selector的主要作用是 标识 和 定位 智能合约中的函数。
+// selector的主要作用 是 标识 和 定位 智能合约中的函数。
 
 // 通过 selector，区块链节点 可以快速 找到 并 调用 目标函数，而 无需 遍历 合约中的所有函数。
